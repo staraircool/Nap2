98cc055ada5ecc9b59cb8aa4d86707696ef5e616
staraircool <295starboy@gmail.com>
2025-07-27 21:52:09 +0500

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,31 +1,47 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("com.google.gms.google-services")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.napcoin_app"
+    compileSdk = 34 // replace with flutter.compileSdkVersion if you're managing it globally
+    ndkVersion = "27.0.12077973" // explicitly fix NDK version
+
+    defaultConfig {
+        applicationId = "com.example.napcoin_app"
+        minSdk = 24 // replace with flutter.minSdkVersion if used
+        targetSdk = 34 // replace with flutter.targetSdkVersion if used
+        versionCode = 1 // replace with flutter.versionCode if used
+        versionName = "1.0" // replace with flutter.versionName if used
     }
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.2")
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug") // change to release config later
+        }
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+flutter {
+    source = "../.."
 }
