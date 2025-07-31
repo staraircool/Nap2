98f9984ec1d1fa7a9549b14f14bf19fe58534038
staraircool <295starboy@gmail.com>
2025-07-31 23:10:36 +0500

    Update auth_gate.dart

--- lib/screens/authenticate/lib/screens/auth_gate.dart
+++ lib/screens/authenticate/lib/screens/auth_gate.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'home/home_screen.dart';
+import 'authenticate/sign_in_screen.dart';
 
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final session = Supabase.instance.client.auth.currentSession;
+    if (session != null) {
+      return const HomeScreen(); // Logged in
+    } else {
+      return const SignInScreen(); // Not logged in
+    }
+  }
+}
